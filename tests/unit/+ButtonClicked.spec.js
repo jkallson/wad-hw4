2c0d78edc39c633775fd68c55379bec2e9dabc43
jannoja <jannojl@gmail.com>
2019-12-15 17:31:37 +0200

    Small fix

--- tests/unit/+ButtonClicked.spec.js
+++ tests/unit/+ButtonClicked.spec.js
@@ -1,16 +1,14 @@
 // Import the mount() method from the test utils
 // and the component you want to test
 
-import { mount } from '@vue/test-utils'
+import {mount, shallowMount} from '@vue/test-utils'
 import Footer from "../../src/components/Footer.vue";
 
 
 describe('Click event', () => {
     it('Click on + button', () => {
         const wrapper = mount(Footer);
-        wrapper.vm.open = false;
         wrapper.find('span.plusButton').trigger('click');
         expect(wrapper.vm.open).toBe(true);
     });
-
 });
